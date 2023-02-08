71e8854d162e1902e9722fdf9ad468c089b77621
Luca Guillemot <luca.guillemot@gmail.com>
2023-02-08 14:25:08 +0100

    rajout btn

--- JS/home.js
+++ JS/home.js
@@ -14,6 +14,11 @@ export default {
       <h1 class="text-center z-2 my-5">Challenge our Pop Culture</h1>
       <button>Check our last game</button>
     </div>
+
+    <div id="buttons" class="d-flex justify-content-center">
+      <a href ="">Pop-corn game</a>
+      <a id="btn-right" href="">Swipe-game</a>
+    </div>
     
 
 `,
