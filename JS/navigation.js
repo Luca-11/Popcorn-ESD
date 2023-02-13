db55d68b17853928a29972affb2acbde97e209eb
ETHEVE Fabien <fabienetheve@hotmail.com>
2023-02-13 12:17:40 +0100

    Ajustement de la page infos

--- JS/navigation.js
+++ JS/navigation.js
@@ -1,11 +1,12 @@
 export default {
   template: `
-
   <nav class="container">
   <div class="row">
     <div class="col-12 " id="menu">
       <div class="col-10">
+      <a href="https://hub2023.popcorn-esd.com/">
         <img class="img-fluid" src="./img/logoPopcorn.png" alt="logo-popcorn" id="logoPopcorn"/>
+      </a>
       </div>
        <div class="col-2" id="logo">
               <img class="img-fluid logoAnimation" src="./img/esd-logo.png" alt="logo-esd" />
@@ -13,6 +14,5 @@ export default {
     </div>
   </div>
 </nav>
-
 `,
 };
--- JS/pres-game.js
+++ JS/pres-game.js
@@ -1,43 +1,40 @@
-
-export default{
-  data(){
-    return{
-        //on vaire un tableau qui va stocké les data venant de la function async 
-        infoGame:[],
-        infoGroupe:[],
-    }
-},
-props:{
-    id:Number,
-    url:String,
-
-},
-// va recupérer les data dans la rest api et le mettre sous format json
-// ici on va récupérer les données en fonction des id et -1 car on commence a 0
-async created(){
+import navigation from "./navigation.js";
+export default {
+  data() {
+    return {
+      //on vaire un tableau qui va stocké les data venant de la function async
+      infoGame: [],
+      infoGroupe: [],
+    };
+  },
+  props: {
+    id: Number,
+    url: String,
+  },
+  components: { navigation },
+  // va recupérer les data dans la rest api et le mettre sous format json
+  // ici on va récupérer les données en fonction des id et -1 car on commence a 0
+  async created() {
     let resp = await fetch(this.url);
-    let data= await resp.json();
+    let data = await resp.json();
     console.log(data);
-    this.infoGame =  data[this.id-1];
-    this.infoGroupe = data[this.id-1].groupe;
+    this.infoGame = data[this.id - 1];
+    this.infoGroupe = data[this.id - 1].groupe;
     // this.infoGroupe = data[this.props.id].groupe;
- 
-},
+  },
   template: `
-  
   <section id="infos">
           <img id="background" :src="infoGame.Picture" alt="image-jeux"/>
           <div class="gradient"></div>
+          <navigation id="header" ></navigation>
           <a :href="infoGame.lien" id="play" target="_blank">Jouer</a>
           <div class="container">
             <div class="row">
             <div id="contents" class="col-12">
-
                <div class="col-md-12 col-12" id="contentJeux">
                 <div id="left" class="col-md-8 col-12">
                 <h2>{{infoGame.Title}}</h2>
                   <p class="text-white my-5">{{infoGame.Description}}</p>
-                  
                 </div>
                 <div id="right" class="col-md-4 col-12">
                 <h5>Crédits : </h5>
@@ -45,12 +42,8 @@ async created(){
                 </div>
               </div>
             </div>
-           
           </div>
         </div>
         </section>
-  
-  
   `,
-
 };
